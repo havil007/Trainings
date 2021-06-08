01a78afa5291a45b18d37f7cc0508c3beaf537f5
havil007 <85561131+havil007@users.noreply.github.com>
2021-06-08 18:42:53 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,20 +1,6 @@
-#!/usr/bin/env bash
-# File: guessinggame.sh
-
 dirpath=($(ls -d */))
 filecount=${#dirpath[@]}
 dirguess=0
-
-function verifyguess 
-{
-    if [[ $1 -lt $2 ]]
-    then
-    echo "Guess is lesser than expected"
-    elif [[ $1 -gt $2 ]]
-    then
-    echo "Guess is higher than expected"
-    fi
-}
 
 while [ $dirguess -ne $filecount ]
 do
@@ -22,4 +8,15 @@ echo "Guess the number of files in the directory!"
 read dirguess
 $(verifyguess $dirguess $filecount)
 done
-echo "Congratulations"
+
+function verifyguess 
+{
+    if [[ $1 -lt $2 ]]
+    then echo "Guess is lesser than expected"
+    elif [[ $1 -gt $2 ]]
+    then echo "Guess is higher than expected"
+    elif
+    then echo "Congratulations!!"
+}
+
+
